32dfc5e5675f3b258e9408d7985e6dffcbccd2b7
Brian Aji Pamungkas <brianajiks123@gmail.com>
2024-12-03 21:19:13 +0700

    add file upload

--- app/Models/Speaker.php
+++ app/Models/Speaker.php
@@ -10,6 +10,12 @@ class Speaker extends Model
     /** @use HasFactory<\Database\Factories\SpeakerFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'bio',
+        'photo',
+        'event_id'
+    ];
     public function event()
     {
         return $this->belongsTo(Event::class);
